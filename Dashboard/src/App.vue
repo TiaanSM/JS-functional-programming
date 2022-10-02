82ebfe17de90ac487f26a5dc833f71b1abf11cbd
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-10-02 17:49:33 +0200

    Researched Vue for the project.
    Started by researching vue and planning out how the app will work.

--- Dashboard/src/App.vue
+++ Dashboard/src/App.vue
@@ -1,32 +1,21 @@
 <template>
-   <h1 @click="handleHeadingClick">My name is {{ name }} ,im {{ age }} years old</h1>
+   <div class="dashboard">
+      <TodoList></TodoList>
+   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import TodoList from './components/TodoList.vue'
 
-const name = ref('Rick')
-const age = ref(21)
-
-const handleHeadingClick = () => {
-  name.value = 'Stefan'
-  age.value = 31
-}
 </script>
 
-<!--
-<script>
-import { ref } from 'vue'
-export default {
-  setup() {
-    const name = ref('Tiaan')
-    const age = ref(21)
-
-    return {
-      name,
-      age
-    }
+<style scoped>
+  
+  .Todo-List {
+    width: 35rem;
+    height: 25rem;
+    background-color: #333;
   }
-}
-</script>
--->
+
+</style>
