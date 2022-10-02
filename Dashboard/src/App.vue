3fee8aea6dd85a1fa3e14cb17d712ed5b8c33aa1
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-10-02 13:53:10 +0200

    Added Starter files for project

---
+++ Dashboard/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+   <h1 @click="handleHeadingClick">My name is {{ name }} ,im {{ age }} years old</h1>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('Rick')
+const age = ref(21)
+
+const handleHeadingClick = () => {
+  name.value = 'Stefan'
+  age.value = 31
+}
+</script>
+
+<!--
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const name = ref('Tiaan')
+    const age = ref(21)
+
+    return {
+      name,
+      age
+    }
+  }
+}
+</script>
+-->
--- README.md
+++
@@ -1,2 +0,0 @@
-# Dashboard
- Dashboard made with Vuejs
