262ae3cac376fcce03b7559b9a27aeb7955dd6fc
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-10-06 16:40:20 +0200

    Removed timer
    Removed the timer app.

--- Dashboard/src/App.vue
+++ Dashboard/src/App.vue
@@ -1,14 +1,11 @@
 <template>
    <div class="dashboard">
       <TodoList class="todoList"></TodoList>
-      <TimerApp></TimerApp>
    </div>
 </template>
 
 <script setup>
 import TodoList from './components/TodoList.vue'
-import TimerApp from './components/TimerApp.vue'
-
 </script>
 
 <style scoped>
